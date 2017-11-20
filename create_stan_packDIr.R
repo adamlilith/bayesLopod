458a70365fc24496d16f33e4785eea87d699fd10
Camilo Sanin <camilosanin@gmail.com>
2017-11-20 18:21:23 -0500

    folder clean up

--- create_stan_packDIr.R
+++
@@ -1,9 +0,0 @@
-library(rstantools)
-library(raster)
-library(rstan)
-
-rFiles = list.files("C:/GitHub/bayesLopod/OriginalData/R", full.names = T)
-stanFiles = list.files("C:/GitHub/bayesLopod/OriginalData/R", full.names = T, pattern = ".stan")
-
-rstan_package_skeleton(name = "bayesLopod",   code_files = rFiles, stan_files = stanFiles, path = "C:/GitHub/bayesLopod" )
-
