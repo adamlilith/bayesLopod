53d0e5e3f0cc92ff27c01cd5c314078a7b2589b3
Camilo Sanin <camilosanin@gmail.com>
2017-11-30 15:14:04 -0600

    Update LopodData-class.R

--- R/LopodData-class.R
+++ R/LopodData-class.R
@@ -1,4 +1,4 @@
-setClassUnion("spatialObjectsUnion", members = c("RasterStack", "SpatialPolygonsDataFrame"))
+setClassUnion("spatialObjectsUnion", members = c("RasterStack", "SpatialPolygonsDataFram
 
 
 #' An S4 class to contain data to be input into a bayesLopod model.
@@ -9,5 +9,7 @@ setClassUnion("spatialObjectsUnion", members = c("RasterStack", "SpatialPolygons
 #' @slot geoDataObject Spatial object supported by bayesLopod
 #' @slot geoType Type of geographical data (Only rasters supported for now)
 #' @slot geoInfo Additional spatial information to be passed to modelLopod
+
+# geoInfo is list, can be data frame with two columns, one with index referncing geographic unit (eg cell number), other is index for STAN (STAN index can be different from geo object order)
 
 LopodData_Class = setClass("LopodData",contains = "spatialObjectsUnion", slots=c(geoDataObject = "spatialObjectsUnion", geoType = "character", geoInfo = "list" ) )
